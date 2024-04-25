c7ab2dcf2701929b1d4aba2f57cd13f16ece1814
James Lowry <jl@balkerne.com>
2024-04-25 13:44:17 -0400

    placeholder endpoint methods

--- blog_api/api.py
+++ blog_api/api.py
@@ -1,6 +1,18 @@
-#TODO: Implement the CRUD endpoints for a blog post
+from blog_api.constants import app
 
+@app.get('/blogs')
+def list_blogs():
+    return "look at all these blog posts"
+    
+@app.post("/blogs")
+def create_blog():
+    return "blog post created"
 
+@app.delete("/blogs/<int:blog_id>")
+def test(blog_id):
+    return f"deleted post {blog_id}"
 
-
+@app.put("/blogs/<int:blog_id>")
+def update_blog(blog_id):
+    return f"update {blog_id}"
 
