3a73990532da87aaa24b7c774e0ad82d5ee8f97c
James Lowry <jl@balkerne.com>
2024-04-28 17:15:31 -0400

    all crud endpoints work

--- blog_api/api.py
+++ blog_api/api.py
@@ -3,7 +3,15 @@ from blog_api.models import BlogPost, User
 from flask import request, render_template, redirect, url_for
 from flask_login import login_user, current_user, login_required, logout_user
 from sqlalchemy import update, and_
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField, TextAreaField
 import secrets
+
+class BlogForm(FlaskForm):
+    title = StringField("Title")
+    subtitle = StringField("Subtitle")
+    body = TextAreaField("Body")
+    submit = SubmitField("Save Changes")
 
 
 @app.get('/blogs')
@@ -13,12 +21,14 @@ def list_blogs():
 
 @app.get("/blogs/create")
 def new_blog_form():
-    return render_template("new_blog.html")
+    form = BlogForm()
+    return render_template("new_blog.html", form=form)
 
 @app.get('/blogs/<int:blog_id>')
 def get_blog(blog_id):
     res = db.get_or_404(BlogPost, blog_id)
     blog = {
+        "id": res.id,
         "title": res.title,
         "subtitle": res.subtitle, 
         "body": res.body,
@@ -26,8 +36,6 @@ def get_blog(blog_id):
         "updated_at": res.updated_at,
         "author": res.author.username
     }
-    print(f"Current user {current_user}")
-    print(f"Is authed {(current_user.is_authenticated and current_user.id==res.author.id)}")
     return render_template('./blog.html', blog=blog, is_authed=(current_user.is_authenticated and current_user.id==res.author.id))
     
 @app.post("/blogs/create")
@@ -35,23 +43,29 @@ def get_blog(blog_id):
 def create_blog():
     blog_details = request.form
     try:
-        new_blog = BlogPost(**blog_details, author_id=int(current_user.id))
+        new_blog = BlogPost(title=blog_details["title"], subtitle=blog_details["subtitle"], body=blog_details["body"],  author_id=int(current_user.id))
         db.session.add(new_blog)
         db.session.commit()
-        return redirect("/blogs")
+        return redirect(url_for("get_blog", blog_id=new_blog.id))
     except Exception as e:
         print(e)
         return "Something went wrong"
 
-@app.delete("/blogs/<int:blog_id>")
+@app.get("/blogs/<int:blog_id>/delete")
 @login_required
 def delete_blog(blog_id):
     res = db.session.execute(BlogPost.__table__.delete().where(and_(BlogPost.id==blog_id, BlogPost.author_id==int(current_user.id))))
     db.session.commit()
-    print(res)
-    return f"deleted post {blog_id}"
+    return redirect("/blogs")
 
-@app.put("/blogs/<int:blog_id>")
+@app.get("/blogs/<int:blog_id>/edit")
+@login_required
+def edit_blog_form(blog_id):
+    to_update = db.get_or_404(BlogPost, blog_id)
+    form = BlogForm(obj=to_update)
+    return render_template("./edit_blog.html", form=form, blog_id=to_update.id)
+
+@app.post("/blogs/<int:blog_id>/edit")
 @login_required
 def update_blog(blog_id):
     """
@@ -60,28 +74,12 @@ def update_blog(blog_id):
     https://stackoverflow.com/questions/1049293/sqlite-error-cannot-commit-transaction-sql-statements-in-progress-using-java
     """
     #BUG: Potential bug, if only provide one field, only that field+created_at & updated_at are returned
-    updated = request.get_json()
-    stmt = update(BlogPost.__table__).where(and_(BlogPost.id==blog_id, BlogPost.author_id==int(current_user.id))).values({key: value for key, value in updated.items() if key not in ("id", "created_at", "author_id")}).returning(BlogPost.__table__.c.created_at, BlogPost.__table__.c.updated_at) 
-    res = list(db.session.execute(stmt))
-    #values of res have to be used before the commit or an error is thrown
-    res = {**updated, "created_at": res[0].created_at, "updated_at": res[0].updated_at}
+    updated = request.form
+    print(f"Updated {updated}")
+    stmt = update(BlogPost.__table__).where(and_(BlogPost.id==blog_id, BlogPost.author_id==int(current_user.id))).values({key: value for key, value in updated.items() if key in ("title", "subtitle", "body")})
+    res = db.session.execute(stmt)
     db.session.commit()
-    return res
-
-
-@app.get("/blogs/seed")
-def seed_db():
-    for i in range(10):
-        #the generated id is 1-indexed so this keeps the other info in line with the id
-        id = i+1
-        blog = BlogPost()
-        blog.title = f"Title {id}"
-        blog.subtitle = f"Subtitle {id}"
-        blog.body = f"Body {id}"
-        blog.author_id = 1
-        db.session.add(blog)
-    db.session.commit()
-    return "DB successfully seeded"
+    return redirect(url_for("get_blog", blog_id=blog_id))
 
 
 @app.post("/signup")
@@ -99,8 +97,6 @@ def get_login():
 @app.post("/login")
 def login():
     username, password = request.form.values()
-    print(f"Username {username}")
-    print(f"password {password}")
     user = User.query.filter(and_(User.username==username, User.password==password)).first()
     if(user):
         #redirect to main page
@@ -124,3 +120,18 @@ def get_auth_status():
     return {
         "current_user": None if current_user.is_anonymous else current_user.username
     }
+
+
+@app.get("/blogs/seed")
+def seed_db():
+    for i in range(10):
+        #the generated id is 1-indexed so this keeps the other info in line with the id
+        id = i+1
+        blog = BlogPost()
+        blog.title = f"Title {id}"
+        blog.subtitle = f"Subtitle {id}"
+        blog.body = f"Body {id}"
+        blog.author_id = 1
+        db.session.add(blog)
+    db.session.commit()
+    return "DB successfully seeded"
